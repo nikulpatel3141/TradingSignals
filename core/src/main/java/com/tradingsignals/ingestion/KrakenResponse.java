08b32c1d4e20812f944aaa8e24ddf5a86c587881
Nikul Patel <nikulpatel3141@users.noreply.github.com>
2025-09-30 15:14:58 +0100

    refactor KrakenResponse parsing; switch to instant for timestamp

--- core/src/main/java/com/tradingsignals/ingestion/KrakenResponse.java
+++ core/src/main/java/com/tradingsignals/ingestion/KrakenResponse.java
@@ -1,17 +1,30 @@
 package com.tradingsignals.ingestion;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import javax.annotation.Nullable;
 import java.math.BigInteger;
-import java.sql.Timestamp;
+import java.time.Instant;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record KrakenResponse(String channel, @Nullable String type, @Nullable CoinData[] data){
+    static final ObjectMapper mapper = new ObjectMapper().findAndRegisterModules();
 
     @JsonIgnoreProperties(ignoreUnknown = true)
-    record CoinData(String symbol, CoinBookLevel[] bids, CoinBookLevel[] asks, @Nullable BigInteger checksum, @Nullable Timestamp timestamp){}
+    record CoinData(
+            String symbol, CoinBookLevel[] bids, CoinBookLevel[] asks, @Nullable BigInteger checksum,
+            @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSSSS'Z'", timezone = "UTC")
+            @Nullable
+            Instant timestamp
+    ){}
 
     @JsonIgnoreProperties(ignoreUnknown = true)
     record CoinBookLevel(Double price, Double qty){}
+
+    public static KrakenResponse parseMessage(String message) throws JsonProcessingException {
+        return mapper.readValue(message, KrakenResponse.class);
+    }
 }
--- core/src/test/java/com/tradingsignals/ingestion/KrakenResponseTest.java
+++ core/src/test/java/com/tradingsignals/ingestion/KrakenResponseTest.java
@@ -17,11 +17,9 @@ class KrakenResponseTest {
     static final String SNAPSHOT_RESPONSE = "{\"channel\":\"book\",\"type\":\"snapshot\",\"data\":[{\"symbol\":\"ETH/GBP\",\"bids\":[{\"price\":3094.60,\"qty\":1.21399386},{\"price\":3094.59,\"qty\":3.00805560},{\"price\":3094.55,\"qty\":3.00809077},{\"price\":3094.41,\"qty\":3.00822915},{\"price\":3094.24,\"qty\":0.00744000},{\"price\":3094.23,\"qty\":3.00840484},{\"price\":3094.18,\"qty\":30.08448385},{\"price\":3094.12,\"qty\":3.00850871},{\"price\":3093.96,\"qty\":0.08799034},{\"price\":3093.52,\"qty\":1.17740030}],\"asks\":[{\"price\":3094.94,\"qty\":1.21385242},{\"price\":3094.95,\"qty\":3.00770571},{\"price\":3094.98,\"qty\":3.00767655},{\"price\":3095.08,\"qty\":3.00757938},{\"price\":3095.29,\"qty\":3.07697533},{\"price\":3095.53,\"qty\":3.00714812},{\"price\":3095.67,\"qty\":0.06960000},{\"price\":3095.86,\"qty\":0.52165080},{\"price\":3095.94,\"qty\":3.00674750},{\"price\":3095.98,\"qty\":1.94160000}],\"checksum\":2216566255}]}";
     static final String UPDATE_RESPONSE = "{\"channel\":\"book\",\"type\":\"update\",\"data\":[{\"symbol\":\"BTC/USD\",\"bids\":[],\"asks\":[{\"price\":113312.4,\"qty\":16.90080565}],\"checksum\":3289960256,\"timestamp\":\"2025-09-30T13:36:20.775204Z\"}]}";
 
-    static final ObjectMapper mapper = new ObjectMapper();
-
     @Test
     public void testSnapshotParsing() throws JsonProcessingException {
-        var result = mapper.readValue(SNAPSHOT_RESPONSE, KrakenResponse.class);
+        var result = KrakenResponse.parseMessage(SNAPSHOT_RESPONSE);
 
         assertEquals("book", result.channel());
         assertEquals("snapshot", result.type());
@@ -35,7 +33,7 @@ class KrakenResponseTest {
 
     @Test
     public void testUpdateParsing() throws JsonProcessingException {
-        var result = mapper.readValue(UPDATE_RESPONSE, KrakenResponse.class);
+        var result = KrakenResponse.parseMessage(UPDATE_RESPONSE);
 
         assertEquals("book", result.channel());
         assertEquals("update", result.type());
@@ -50,10 +48,7 @@ class KrakenResponseTest {
         assertEquals(16.90080565, firstAsk.qty());
 
         assertEquals(new BigInteger("3289960256"), data.checksum());
-
-//        var dateFormat = new SimpleDateFormat("yyyy-mm-dd")
-
-//        assertEquals(Timestamp.valueOf("2025-09-30T13:36:20.775204Z", ), data.timestamp());
+        assertEquals(Instant.parse("2025-09-30T13:36:20.775204Z"), data.timestamp());
 
     }
 
