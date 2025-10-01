9e7070c03c9851c974bf3d06af5452888ea92b89
Nikul Patel <nikulpatel3141@users.noreply.github.com>
2025-10-01 16:52:26 +0100

    test ask update logic

--- core/src/test/java/com/tradingsignals/models/OrderBookTest.java
+++ core/src/test/java/com/tradingsignals/models/OrderBookTest.java
@@ -14,16 +14,20 @@ class OrderBookTest {
         var initialBook = new OrderBook(2);
 
         var bids = new BookUpdate[]{new BookUpdate(1.0, 12.123), new BookUpdate(1.1, 2.123)};
-        initialBook.applyUpdate(bids, new BookUpdate[]{});
+        initialBook.applyUpdate(bids, bids);
 
         bids = new BookUpdate[]{new BookUpdate(1.05, 1.4)};
-        initialBook.applyUpdate(bids, new BookUpdate[]{});
+        initialBook.applyUpdate(bids, bids);
 
-        // should remove the worst bid
+        // should remove the worst bid + ask
         var bookBids = initialBook.getSide(OrderBook.Side.BIDS);
-
         assertEquals(2, bookBids.size());
         assertEquals(1.05, bookBids.firstKey());
         assertEquals(1.1, bookBids.lastKey());
+
+        var bookAsks = initialBook.getSide(OrderBook.Side.ASKS);
+        assertEquals(2, bookAsks.size());
+        assertEquals(1.0, bookAsks.firstKey());
+        assertEquals(1.05, bookAsks.lastKey());
     }
 }
